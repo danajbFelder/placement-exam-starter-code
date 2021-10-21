fd0c48fd4327cf9ab169f7e8d022317f0820a302
danajbFelder <91923717+danajbFelder@users.noreply.github.com>
2021-10-20 21:44:14 -0400

    add cookie counter for Ginger Bread and Chocolate Chip

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,44 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
+let yourName = "Dana Jasmine Braxton-Felder" // HINT: Replace this with your own name!
+
+// We'll use these variables to track the counts of each cookie type
+let gb = 0      // Ginger bread
+/* if (gb ){ 
+
+}*/
+
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+
+// Code to update name display 
+document.getElementById('credit').textContent = `Created by ${yourName}`
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.getElementById('add-gb').addEventListener('click', function() {
+gb++
+
+document.getElementById("qty-gb").textContent = gb
+
+
+})
+
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc++
+    
+document.getElementById("qty-cc").textContent = cc
+
+})
+    
+// document.getElementById('minus-gb').addEventListener('click', function() {
+// gb--
+
+
+// HINT: You can delete this console.log after you no longer need it!
+console.log('JavaScript code has loaded!')
+
+// First, tell us your name
 let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
